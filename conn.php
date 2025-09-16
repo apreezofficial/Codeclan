991fbed586360ac89a654fc6ea6b5dbef3c24f42
apreezofficial <apreezofficial@gmail.com>
2025-09-16 08:47:41 +0100

    Installed vulcas phpdotenv and connected to pxxl mysql db .. Final steps Db connected Pages loading up Includes workding Admin panel in progress manual uploads active

--- conn.php
+++ conn.php
@@ -5,11 +5,12 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
 $db   = $_ENV['DB_NAME'];
 $user = $_ENV['DB_USER'];
 $pass = $_ENV['DB_PASS'];
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
