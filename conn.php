58db8183b1f34dd5dedfd1b1ec4a1cf35d98c6a6
Precious Adedokun <apreezofficial@gmail.com>
2025-09-16 08:33:53 +0100

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,8 +1,13 @@
 <?php
-$host = '0.0.0.0';
-$db   = 'codeclan';
-$user = 'root'; 
-$pass = 'root';  
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 $conn = new mysqli($host, $user, $pass, $db);
 
